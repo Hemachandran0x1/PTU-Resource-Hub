1b74b6a5e223550634f671c8165bb88ead8db640
Hemachandran0x1 <phemachandran173@gmail.com>
2023-05-24 22:47:27 +0530

    update

--- user-service/src/main/java/com/mscomm/userservice/service/SemesterService.java
+++
@@ -1,7 +0,0 @@
-package com.mscomm.userservice.service;
-import java.util.List;
-import com.mscomm.userservice.entity.*;
-public interface SemesterService {
-	Semester getSemesterbyid(long id);
-	List<Semester> getallSemesters();
-}
