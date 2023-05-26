72cf443d1ca938d72905e42d6342520136fbdd89
Gokulnath-Pugazhendi <gokulabhimany@gmail.com>
2023-05-26 15:52:21 +0530

    deleted backend for updation

--- backend/resource-service/src/main/java/com/mscomm/resourceservice/repository/ContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mscomm.resourceservice.repository;
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mscomm.resourceservice.entity.Content;
-public interface ContentRepository extends JpaRepository<Content,Long>{
-	List<Content> findByTopicidAndType(Long topic,int type);
-}
