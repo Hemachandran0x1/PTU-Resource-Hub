52a5b317226cc6e8458d9078ea12e25e7fb7c3c1
Hemachandran0x1 <phemachandran173@gmail.com>
2023-05-28 16:58:43 +0530

    update

--- backend/user-service/src/main/java/com/mscomm/userservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.mscomm.userservice.service;
-import com.mscomm.userservice.entity.*;
-
-//import com.mscomm.userservice.dto.ResponseDto;
-public interface UserService {
-	User register(User user);
-	User login(String username,String password);
-   // ResponseDto getUser(Long userId);
-}
